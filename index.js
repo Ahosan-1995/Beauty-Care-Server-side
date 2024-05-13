d78920f53e1f810f4b350f5f2b83bd4422fd8f96
Ahosan-1995 <ahsoanhabibshakil2@gmail.com>
2024-05-14 00:45:02 +0600

    dynamic route data collected from database

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ async function run() {
     })
 
 
+    app.get('/assignment/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await allDataCollection.findOne(query);
+        res.send(result);
+    })
+
+
 
 
 
